c67ee4833da845082a16961353455f5a231d3e54
alsekaram <git@awl.su>
2025-11-28 10:29:09 +0300

    refactor: Update Yandex Metrica integration to use the `/watch` endpoint with revised parameters and URL structure.

--- src/analytics.py
+++ src/analytics.py
@@ -39,42 +39,46 @@ async def track_yandex_hit(
         client_id_source = f"{ip_address or ''}{user_agent or ''}"
         client_id = hashlib.md5(client_id_source.encode()).hexdigest()
 
-        # Prepare parameters for Measurement Protocol
-        # https://yandex.com/support/metrica/code/measurement-protocol.html
+        # Use the "watch" endpoint which mimics the pixel/noscript tracking
+        # This is more robust when no token is available
+        # https://mc.yandex.ru/watch/{counter_id}
         
         # Construct the "virtual" URL that Yandex will see
-        # We append the query params here so Yandex sees ?yqrid=... etc.
         tracked_url = f"{settings.base_url}/{short_code}"
         if params:
             from urllib.parse import urlencode
             tracked_url += f"?{urlencode(params)}"
 
+        # Parameters for /watch endpoint
         params_body = {
-            "tid": settings.yandex_metrica_counter_id,
-            "cid": client_id,
-            "url": tracked_url,
-            "referer": referer or "",
-            "ua": user_agent or "",
-            "uip": ip_address or "",  # User IP (requires token)
+            "page-url": tracked_url,
+            "page-ref": referer or "",
+            "charset": "utf-8",
+            "browser-info": f"u:{user_agent or ''}",  # Basic browser info
         }
         
-        # Add title if possible, or just use short code
-        params_body["dt"] = f"Short Link: {short_code}"
+        # Add title
+        params_body["title"] = f"Short Link: {short_code}"
 
-        # If we have a token, we can send IP address for better geo-location
-        # Note: 'uip' parameter requires the 'ms' (token) parameter to work
-        if settings.yandex_metrica_token:
-            params_body["ms"] = settings.yandex_metrica_token
-        else:
-            # If no token, remove uip as it won't be processed
-            params_body.pop("uip", None)
+        # If we have a token, we might be able to pass more info, but for /watch
+        # it primarily relies on IP (which we can't easily spoof without token)
+        # and cookies (which we don't have).
+        # We'll just send the hit.
 
         async with httpx.AsyncClient() as client:
+            # We use the counter ID in the path
+            url = f"https://mc.yandex.ru/watch/{settings.yandex_metrica_counter_id}"
+            
             response = await client.get(
-                "https://mc.yandex.ru/watch",
+                url,
                 params=params_body,
                 timeout=5.0
             )
+            
+            if response.status_code == 200:
+                logger.debug(f"Yandex Metrica hit sent for {short_code} to {url}")
+            else:
+                logger.warning(f"Yandex Metrica error: {response.status_code} {response.text}")
             
             if response.status_code == 200:
                 logger.debug(f"Yandex Metrica hit sent for {short_code}")
